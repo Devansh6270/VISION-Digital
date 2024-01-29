af0ae890ce222e7810b79e37eb07cefc371c5dfd
Yogi1606 <tripathinihal6@gmail.com>
2024-01-29 13:40:27 +0530

    useless commit 29Jan

---
+++ app/src/main/java/com/vision_digital/activities/InstallmentOrderListActivity.java
@@ -0,0 +1,16 @@
+package com.vision_digital.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vision_digital.R;
+
+public class InstallmentOrderListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_installment_order_list);
+    }
+}
